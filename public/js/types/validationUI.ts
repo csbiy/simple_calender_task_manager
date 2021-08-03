a56298037bf5dc535600a8a48fee3f6a6a774533
BioCSK <katd6@naver.com>
2021-08-03 17:31:33 +0900

    update

--- public/js/types/validationUI.ts
+++ public/js/types/validationUI.ts
@@ -2,10 +2,10 @@ import {Bootstrap} from "./bootstrapUI";
 
 class ValidationUI{
 
-    public static PASS_BORDER_COLOR : Bootstrap.GREEN_BORDER;
-    public static FAIL_BORDER_COLOR : Bootstrap.RED_BORDER;
-    public static PASS_TEXT_COLOR: Bootstrap.GREEN_BORDER;
-    public static FAIL_TEXT_COLOR: Bootstrap.RED_BORDER;
+    public static PASS_BORDER_COLOR = Bootstrap.GREEN_BORDER;
+    public static FAIL_BORDER_COLOR = Bootstrap.RED_BORDER;
+    public static PASS_TEXT_COLOR =  Bootstrap.GREEN_BORDER;
+    public static FAIL_TEXT_COLOR = Bootstrap.RED_BORDER;
 
 }
 
